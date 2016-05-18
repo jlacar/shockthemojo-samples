e539c54305e19f5701ff127151c2b789c3c6cc7a
Junilu Lacar <jlacar@cisco.com>
2016-05-18 17:22:07 -0400

    Simplified test code

--- tdd-demo/src/test/java/com/shockthemojo/tdd/MaxMinArrangerTest.java
+++ tdd-demo/src/test/java/com/shockthemojo/tdd/MaxMinArrangerTest.java
@@ -5,36 +5,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MaxMinArrangerTest {
-	
+
+	private MaxMinArranger testSubject = new MaxMinArranger();
+
 	@Test
 	public void empty_array_returns_empty_result() throws Exception {
-		MaxMinArranger testSubject = new MaxMinArranger();
-		int[] result = testSubject.arrange();
-		
-		assertArrayEquals(new int[] {}, result);
+		assertArrayEquals(new int[] {}, testSubject.arrange());
 	}
-	
+
 	@Test
 	public void two_element_array_comes_back_with_max_min() {
-		MaxMinArranger testSubject = new MaxMinArranger();
-		int[] result = testSubject.arrange(1, 7);
-		
-		assertArrayEquals(new int[] {7, 1}, result);
+		assertArrayEquals(new int[] { 7, 1 }, testSubject.arrange(1, 7));
 	}
 
 	@Test
 	public void multiple_elements_arranged_as_max_min_pairs() throws Exception {
-		MaxMinArranger testSubject = new MaxMinArranger();
-		int[] result = testSubject.arrange(6, 2, 1, 7);
-		
-		assertArrayEquals(new int[] {7, 1, 6, 2}, result);
+		assertArrayEquals(new int[] { 7, 1, 6, 2 }, testSubject.arrange(6, 2, 1, 7));
 	}
-	
+
 	@Test
 	public void tried_with_sample_data_given_in_problem() throws Exception {
-		MaxMinArranger testSubject = new MaxMinArranger();
-		int[] result = testSubject.arrange(6, 4, 1, 7, 3, 2, 5);
-		
-		assertArrayEquals(new int[] {7, 1, 6, 2, 5, 3, 4}, result);
+		assertArrayEquals(new int[] { 7, 1, 6, 2, 5, 3, 4 }, testSubject.arrange(6, 4, 1, 7, 3, 2, 5));
 	}
 }
