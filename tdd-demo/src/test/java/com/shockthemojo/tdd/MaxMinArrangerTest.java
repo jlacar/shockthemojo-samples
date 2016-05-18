5abb1c02d523abdbfc76aa09b2073f198a9a1e0c
Junilu Lacar <jlacar@cisco.com>
2016-05-18 16:42:29 -0400

    Added a first test class

---
+++ tdd-demo/src/test/java/com/shockthemojo/tdd/MaxMinArrangerTest.java
@@ -0,0 +1,14 @@
+package com.shockthemojo.tdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MaxMinArrangerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
