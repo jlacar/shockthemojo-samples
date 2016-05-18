d7f4ea83977fbfe4a5c5c65e62d483abdad9a680
Junilu Lacar <jlacar@cisco.com>
2016-05-18 16:56:10 -0400

    Green

--- tdd-demo/src/main/java/com/shockthemojo/tdd/MaxMinArranger.java
+++ tdd-demo/src/main/java/com/shockthemojo/tdd/MaxMinArranger.java
@@ -3,7 +3,9 @@ package com.shockthemojo.tdd;
 public class MaxMinArranger {
 
 	public int[] arrange(int... nums) {
-
+		if (nums.length == 0) {
+			return new int[] {};
+		}
 		
 		return new int[] {7, 1};
 	}
