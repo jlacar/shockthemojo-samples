2564d007ecc2def69ac5b624380b0615116b1da4
Junilu Lacar <jlacar@cisco.com>
2016-05-18 21:57:57 -0400

    Added code to make test pass - throwing IllegalArgumentException on null input

--- tdd-demo/src/main/java/com/shockthemojo/tdd/AdjacentMultiplesProblem.java
+++ tdd-demo/src/main/java/com/shockthemojo/tdd/AdjacentMultiplesProblem.java
@@ -3,6 +3,9 @@ package com.shockthemojo.tdd;
 public class AdjacentMultiplesProblem {
 
 	public boolean check(int[] numbers) {
+		if (numbers == null) {
+			throw new IllegalArgumentException("Argument cannot be null.");
+		}
 		return false;
 	}
 
