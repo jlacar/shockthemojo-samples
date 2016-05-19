d31b9e12e9fe7e8f0df407e08d8ecb79832221e4
Junilu Lacar <jlacar@cisco.com>
2016-05-18 21:49:57 -0400

    Added new challenge: Adjacent Multiple

---
+++ tdd-demo/src/test/java/com/shockthemojo/tdd/AdjacentMultiplesProblemTest.java
@@ -0,0 +1,20 @@
+package com.shockthemojo.tdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdjacentMultiplesProblemTest {
+
+	/*
+	 * Given: int[] numbers
+	 * 
+	 * Required: A way to check whether or not the array contains at least one
+	 * pair of adjacent elements such that nums[i] * 10 == nums[i+1]
+	 * 
+	 * Your solution may be iterative or recursive but it must be able to handle
+	 * different lengths of arrays and it should reject a null reference as
+	 * invalid input.
+	 */
+
+}
